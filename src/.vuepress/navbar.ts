c210b516aa36cc59e4209351e7379655ce1bb2b3
ageno <2519529119@qq.com>
2026-05-23 15:41:05 +0800

    提交修改

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -13,20 +13,42 @@ export default navbar([
         icon: "pen-to-square",
         prefix: "c++/",
         children: [
+          //下面还是文件夹 
+          // 子菜单
+          // 这里的link是相对于prefix的路径
           {
             text: "基础",
             icon: "pen-to-square",
-            link: "1",
+            prefix: "基础/",
+            children: [
+              {
+                text: "cpp98",
+                icon: "pen-to-square",
+                link: "01_cpp98_complete_guide", // 链接,或者md的名称
+              },
+              {
+                text: "现代c++新特性",
+                icon: "pen-to-square",
+                link: "02_modern_cpp_features", // 链接,或者md的名称
+              },
+              {
+                text: "文件操作和io",
+                icon: "pen-to-square",
+                link: "03_file_io_and_streams", // 链接,或者md的名称
+              },
+            ],
           },
           {
             text: "qt",
             icon: "pen-to-square",
-            link: "2",
-          },
-          {
-            text: "socket",
-            icon: "pen-to-square",
-            link: "3",
+            prefix: "qt/",
+            children: [
+              {
+                text: "qt教程",
+                icon: "pen-to-square",
+                link: "qt_detailed_study_guide", // 链接,或者md的名称
+              },
+            ],
           },
         ],
       },
